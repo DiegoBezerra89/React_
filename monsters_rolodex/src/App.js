d2308502f7a9ac75735efc5cb4e895216334e470
DiegoMartinsWebjump <diego.martins@webjump.com.br>
2022-03-14 18:15:09 -0300

    basics

--- monsters_rolodex/src/App.js
+++ monsters_rolodex/src/App.js
@@ -1,25 +1,36 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			//it's always a json object
+			country : 'Brasil'
+		}
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Hello {this.state.country}
+					</p>
+					<button onClick={() => {
+						this.setState({
+							country:'Jamaica'
+						});
+					}}>
+						Change name
+					</button>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
