f419979c073b79d2e8c5648f3305b721fd5441dd
Diego Bezerra Martins <diegobmartins21@gmail.com>
2022-03-15 11:59:13 -0300

    ComponentDidMount

--- monsters_rolodex/src/App.js
+++ monsters_rolodex/src/App.js
@@ -1,5 +1,4 @@
 import { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -8,45 +7,35 @@ class App extends Component {
 
     this.state = {
       //it's always a json object
-      name: {
-        firstName: "Renata",
-        lastName: "Almeida",
-      },
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello, I'm {this.state.name.firstName} {this.state.name.lastName}!
-          </p>
-          <button
-            onClick={() => {
-              //o primeiro parâmetro do useState é o novo estado
-              this.setState(
-                () => {
-                  return {
-                    name: {
-                      firstName: "Diego",
-                      lastName: "Martins",
-                    },
-                  };
-                },
-                //o segundo parâmetro só vai rodar assim que o estado for atualizado
-                () => {
-                  console.log(this.state);
-                }
-              );
-            }}
-            //O react nota que o estado mudou, porém ele precisa que seja um novo objeto, com a mesma chave em questão
-            //assim que é passado um novo objeto ele entende que o estado mudou, e renderiza o componente com o novo
-          >
-            Change Persons
-          </button>
-        </header>
+        {this.state.monsters.map((item) => {
+          return (
+            <div key={item.id}>
+              <h1>{item.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
